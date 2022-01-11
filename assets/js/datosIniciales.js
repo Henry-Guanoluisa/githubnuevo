d505677e97695035afebfbcf86eb4a4437107dd3
Jonathan Vargas <jonathan.1996mds@gmail.com>
2022-01-10 21:01:48 -0500

    examen inicio

---
+++ assets/js/datosIniciales.js
@@ -0,0 +1,31 @@
+class Datos {
+
+    data = {
+        post1: {
+            titulo: "Prueba de titulo 1 ",
+            publicado: "Publicado el dia 10 de enero del 2022",
+            descripcion: "The JSON format is syntactically similar to the code for creating JavaScript objects. Because of this, a JavaScript program can easily convert JSON data into JavaScript objects."
+        },
+        post2: {
+            titulo: "Prueba de titulo 2 ",
+            publicado: "Publicado el dia 10 de enero del 2022",
+            descripcion: "The JSON format is syntactically similar to the code for creating JavaScript objects. Because of this, a JavaScript program can easily convert JSON data into JavaScript objects."
+        }
+        ,
+        post3: {
+            titulo: "Prueba de titulo 3 ",
+            publicado: "Publicado el dia 10 de enero del 2022",
+            descripcion: "The JSON format is syntactically similar to the code for creating JavaScript objects. Because of this, a JavaScript program can easily convert JSON data into JavaScript objects."
+        },
+        post4: {
+            titulo: "Prueba de titulo 4",
+            publicado: "Publicado el dia 10 de enero del 2022",
+            descripcion: "The JSON format is syntactically similar to the code for creating JavaScript objects. Because of this, a JavaScript program can easily convert JSON data into JavaScript objects."
+        },
+        post5: {
+            titulo: "Prueba de titulo 5 ",
+            publicado: "Publicado el dia 10 de enero del 2022",
+            descripcion: "The JSON format is syntactically similar to the code for creating JavaScript objects. Because of this, a JavaScript program can easily convert JSON data into JavaScript objects."
+        }
+    }
+}
